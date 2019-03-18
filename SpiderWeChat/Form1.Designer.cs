00dcffdf80d4c4a3a77d5b0626bdeeeb8afbb53c
Harry <harryluo163@163.com>
2019-03-18 16:03:04 +0800

    添加项目文件。

---
+++ SpiderWeChat/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SpiderWeChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_CloseConnect = new System.Windows.Forms.Button();
+            this.txt_Msg = new System.Windows.Forms.TextBox();
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(552, 25);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 49);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "开始";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_CloseConnect
+            // 
+            this.btn_CloseConnect.Location = new System.Drawing.Point(680, 28);
+            this.btn_CloseConnect.Name = "btn_CloseConnect";
+            this.btn_CloseConnect.Size = new System.Drawing.Size(75, 42);
+            this.btn_CloseConnect.TabIndex = 1;
+            this.btn_CloseConnect.Text = "停止";
+            this.btn_CloseConnect.UseVisualStyleBackColor = true;
+            this.btn_CloseConnect.Click += new System.EventHandler(this.btn_CloseConnect_Click_1);
+            // 
+            // txt_Msg
+            // 
+            this.txt_Msg.Location = new System.Drawing.Point(43, 118);
+            this.txt_Msg.Multiline = true;
+            this.txt_Msg.Name = "txt_Msg";
+            this.txt_Msg.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_Msg.Size = new System.Drawing.Size(730, 530);
+            this.txt_Msg.TabIndex = 2;
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(104, 40);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(401, 21);
+            this.txt_IP.TabIndex = 3;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(41, 43);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(53, 12);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "服务器：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 694);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txt_IP);
+            this.Controls.Add(this.txt_Msg);
+            this.Controls.Add(this.btn_CloseConnect);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "Form1";
+            this.Text = "微信";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_CloseConnect;
+        private System.Windows.Forms.TextBox txt_Msg;
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ SpiderWeChat/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocket4Net;
+
+namespace SpiderWeChat
+{
+    public partial class Form1 : Form
+    {
+        //计时器  
+        private System.Windows.Forms.Timer tm = new System.Windows.Forms.Timer();
+        //自动重置事件类    
+        //主要用到其两个方法 WaitOne() 和 Set() , 前者阻塞当前线程，后者通知阻塞线程继续往下执行  
+        AutoResetEvent autoEvent = new AutoResetEvent(false);
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            tm.Interval = 1;
+            tm.Tick += new EventHandler(tm_Tick);
+            btn_CloseConnect.Enabled = false;
+        }
+        //计时器 事件  
+        void tm_Tick(object sender, EventArgs e)
+        {
+            if (txt_Msg.Lines.Count() > 100)
+            {
+                txt_Msg.Text = "";
+                txt_Msg.AppendText("清空缓存" + Environment.NewLine);
+            }
+            autoEvent.Set(); //通知阻塞的线程继续执行  
+        }
+
+        WebSocket websocket;
+ 
+
+        #region 向外传递数据事件
+        public event Action<string> MessageReceived;
+        #endregion
+        /// <summary>
+        /// 检查重连线程
+        /// </summary>
+        Thread _thread;
+        bool _isRunning = true;
+        /// <summary>
+        /// WebSocket连接地址
+        /// </summary>
+        public string ServerPath { get; set; }
+
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btn_Start.Enabled = false;
+                btn_CloseConnect.Enabled = true;
+               double num = ((DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds) / 1000;
+
+                ServerPath = "wss://ws02.wxb.com/web/8d71945f103e041f9b6f139e39b8b9f6?t="+Convert.ToInt32(num) +"&uid=1857413&suid=0";
+                websocket = new WebSocket4Net.WebSocket("wss://ws02.wxb.com/web/8d71945f103e041f9b6f139e39b8b9f6?t=1552891701&uid=1857413&suid=0");
+                websocket.Opened += WebSocket_Opened;
+                websocket.Closed += WebSocket_Closed;
+                websocket.Error += websocket_Error;
+                websocket.MessageReceived += WebSocket_MessageReceived;
+                Start();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("连接服务端出错:" + ex.ToString());
+            }
+
+        }
+
+        #region "web socket "
+        /// <summary>
+        /// 连接方法
+        /// <returns></returns>
+        public bool Start()
+        {
+            bool result = true;
+            try
+            {
+                websocket.Open();
+
+                this._isRunning = true;
+                this._thread = new Thread(new ThreadStart(CheckConnection));
+                this._thread.Start();
+            }
+            catch (Exception ex)
+            {
+                 txt_Msg.AppendText(ex.ToString() + Environment.NewLine);
+                result = false;
+            }
+            return result;
+        }
+        /// <summary>
+        /// 消息收到事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void WebSocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+             txt_Msg.AppendText( e.Message + Environment.NewLine);
+            string content = GetStr(e.Message, "text", "}");
+            if (!string.IsNullOrEmpty(content)) {
+               txt_Msg.AppendText(content + Environment.NewLine);
+                string regular = ".*1[2|3|4|5|6|7|8][0-9]{9}.*";
+                Regex mobi = new Regex(regular);
+                Match m = mobi.Match(content);
+                if (m.Success) {
+                    content = content.Replace("\"", "").Replace(":", "").Replace("\\n", "");
+                    txt_Msg.AppendText(content + Environment.NewLine);
+
+                    string Path = "down\\船源数据("+ DateTime.Now.ToString("yyyy-MM-dd") + ").txt";
+                    if (!File.Exists(Path))
+                    {
+                        using (new FileStream(Path, FileMode.Create, FileAccess.Write)) { };
+                    }
+                    using (StreamWriter sw = new StreamWriter(Path, true, Encoding.Default))
+                    {
+                        sw.Write("<content>" + content + "</content>\r\n");
+                    }
+                      
+                }
+            }
+         
+       
+            //MessageReceived?.Invoke(e.Message);
+        }
+        /// <summary>
+        /// Socket关闭事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void WebSocket_Closed(object sender, EventArgs e)
+        {
+             txt_Msg.AppendText("websocket_Closed" + Environment.NewLine);
+        }
+
+
+        /// <summary>
+        /// Socket报错事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void websocket_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
+        {
+            txt_Msg.AppendText("websocket_Error:");
+        }
+        /// <summary>
+        /// Socket打开事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void WebSocket_Opened(object sender, EventArgs e)
+        {
+             txt_Msg.AppendText(" websocket_Opened");
+        }
+        /// <summary>
+        /// 检查重连线程
+        /// </summary>
+        private void CheckConnection()
+        {
+            do
+            {
+                try
+                {
+                    if (websocket.State != WebSocket4Net.WebSocketState.Open && websocket.State != WebSocket4Net.WebSocketState.Connecting)
+                    {
+                         txt_Msg.AppendText(" Reconnect websocket WebSocketState:" + websocket.State);
+                        websocket.Close();
+                        websocket.Open();
+                        Console.WriteLine("正在重连");
+                    }
+                }
+                catch (Exception ex)
+                {
+                     txt_Msg.AppendText(ex.ToString());
+                }
+                System.Threading.Thread.Sleep(5000);
+            } while (this._isRunning);
+        }
+        #endregion
+
+
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="Message"></param>
+        public void SendMessage(string Message)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                if (websocket != null && websocket.State == WebSocket4Net.WebSocketState.Open)
+                {
+                    websocket.Send(Message);
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            this._isRunning = false;
+            try
+            {
+                _thread.Abort();
+            }
+            catch
+            {
+
+            }
+            websocket.Close();
+            websocket.Dispose();
+            websocket = null;
+        }
+
+
+    
+
+        /// <summary>
+        /// 断开连接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_CloseConnect_Click_1(object sender, EventArgs e)
+        {
+            btn_Start.Enabled = true;
+            btn_CloseConnect.Enabled = false;
+            Dispose();
+
+
+        }
+
+        public string GetStr(string pContent, string regBegKey, string regEndKey)
+        {
+            string regstr = "";
+            string regular = "(?<={0})(.|\n)*?(?={1})";
+            regular = string.Format(regular, regBegKey, regEndKey);
+            Regex regex = new Regex(regular, RegexOptions.IgnoreCase);
+            Match m = regex.Match(pContent);
+            if (m.Length > 0)
+            {
+                regstr = m.Value.Trim();
+            }
+            return regstr;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
